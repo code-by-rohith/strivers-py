86bf936cefd17d69b3f2b1e0ba1b5b938f2321e6
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-15 23:50:29 +0530

    add

---
+++ bubblesort.py
@@ -0,0 +1,11 @@
+arr = [88, 22, 65, 95, 25]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    print(arr)
+
+bubble_sort(arr)
