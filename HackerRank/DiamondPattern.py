f6014f638e9b21ef5d08cd5368fd2edcbde6a630
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-18 15:04:28 +0530

    add

--- HackerRank/DiamondPattern.py
+++ HackerRank/DiamondPattern.py
@@ -2,7 +2,7 @@ def print_diamond(n):
     for i in range(n):
         for j in range(n - i - 1):
             print(' ', end='')
-        for j in range(2 * i + 1):
+        for j in range(2 * i +1):
             print('*', end='')
         print()
     for i in range(n - 2, -1, -1):
