4b58547b9d10c11964944b3fb51635de457dc6af
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-23 23:47:39 +0530

    add

---
+++ Easy/Anagrams.py
@@ -0,0 +1,22 @@
+def find_anagram(s, q):
+    max_count = 0
+    similar = None
+
+    for string in s:
+        curr_count = 0
+        for i in range(len(string) - 1):
+            for j in range(len(q) - 1):
+                if string[i] == q[j] and string[i + 1] == q[j + 1]:
+                    curr_count += 1
+
+        if max_count < curr_count:
+            max_count = curr_count
+            similar = string
+
+    return similar
+
+s = ["hello", "hey!!", "yellow"]
+q = input()
+
+res = find_anagram(s, q)
+print("The answer is:", res)
