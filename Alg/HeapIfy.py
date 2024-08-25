581df4ffa420fee47893a6fdd20624bcb7b8bd15
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-25 16:48:36 +0530

    add

--- Alg/HeapIfy.py
+++ Alg/HeapIfy.py
@@ -2,10 +2,13 @@ def heapify(arr, n, i):
     smallest = i
     left = 2 * i + 1
     right = 2 * i + 2
+
     if left < n and arr[left] < arr[smallest]:
         smallest = left
+
     if right < n and arr[right] < arr[smallest]:
         smallest = right
+
     if smallest != i:
         arr[i], arr[smallest] = arr[smallest], arr[i]
         heapify(arr, n, smallest)
@@ -15,6 +18,7 @@ def build_heap(arr):
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
 
-li = [5, 7, 9, 1, 3]
+li = [1, 7, 9, 5, 3,8,65]
+print("Old Heap is        :", li)
 build_heap(li)
 print("the created heap is:", li)
